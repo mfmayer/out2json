85fd601daa09c79ca9734c76cc8be6e3929f63a9
Matthias Mayer <matthias.ma.mayer@daimler.com>
2020-08-18 13:18:22 +0200

    Initial commit of source code

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # stdjson
+
 stdjson converts an application output to **stdout** and **stderr** into json format with each line getting an index number.
+
+## Install
+
+stdjson is written in go and can therefore be installed via: ``go get github.com/mfmayer/mfgrep`` (go must be installed).
+
+## Usage of stdjson
+
+```
+$ ./stdjson 
+./stdjson: missing command
+Usage: ./stdjson COMMAND ARGUMENTS...
+ Try : ./stdjson ls -al
+```
+
+## Example
+
+Reason for writing this tool was logging of service output to e.g. azure log analytics. Microsoft's log analytics agent is able to log any output (stdout & stderr) into the azure log analytics container logs and stores line by line with a timestamp in milliesecond resolution. Unfortunately, this can cause the order of output to be lost if the output is faster, e.g. in case of a panic when outputting a stack trace.
+
+For this reason stdjson wraps every line of output into the following json format: ``{"i": <line index>,"d": <line data/content>}``
+
+```
+$ ./stdjson ls -al
+{"d":"total 2688","i":"1"}
+{"d":"drwxrwxr-x 3 mayema mayema    4096 Aug 18 13:07 .","i":"2"}
+{"d":"drwxr-xr-x 8 mayema mayema    4096 Aug 18 12:57 ..","i":"3"}
+{"d":"drwxrwxr-x 8 mayema mayema    4096 Aug 18 12:57 .git","i":"4"}
+{"d":"-rw-rw-r-- 1 mayema mayema     269 Aug 18 12:57 .gitignore","i":"5"}
+{"d":"-rw-rw-r-- 1 mayema mayema    1071 Aug 18 12:57 LICENSE","i":"6"}
+{"d":"-rw-rw-r-- 1 mayema mayema    2322 Aug 18 13:07 main.go","i":"7"}
+{"d":"-rw-rw-r-- 1 mayema mayema     135 Aug 18 12:57 README.md","i":"8"}
+{"d":"-rwxrwxr-x 1 mayema mayema 2722213 Aug 18 13:07 stdjson","i":"9"}
+```
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	lineCount int64 = 0
+)
+
+const (
+	lineIndexProperty string = "i"
+	lineDataProoperty string = "d"
+)
+
+func printUsage() {
+	fmt.Printf("%v: missing command\n", os.Args[0])
+	fmt.Printf("Usage: %v COMMAND ARGUMENTS...\n", os.Args[0])
+	fmt.Printf(" Try : %v ls -al\n", os.Args[0])
+}
+
+// write creates a line inder and wraps data into json format before writing it to out
+func write(data interface{}, out io.Writer) {
+	lineNo := strconv.Itoa(int(atomic.AddInt64(&lineCount, 1)))
+	entry := map[string]interface{}{
+		lineIndexProperty: lineNo,
+		lineDataProoperty: data,
+	}
+	marshalledEntry, _ := json.Marshal(entry)
+	out.Write(marshalledEntry)
+	out.Write([]byte("\n"))
+}
+
+// processStream reads line by line from given reader and quits if reader is closed or EOF is reached
+func processStream(r io.Reader, out io.Writer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	br := bufio.NewReader(r)
+	for {
+		line, err := br.ReadBytes('\n')
+		if lineLen := len(line); lineLen > 0 {
+			if line[lineLen-1] == '\n' {
+				line = line[:lineLen-1]
+			}
+		}
+		if len(line) > 0 {
+			write(string(line), out)
+		}
+		if err != nil {
+			break
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		printUsage()
+		os.Exit(0)
+	}
+
+	// get parameters and prepare command
+	params := []string{}
+	if len(os.Args) > 1 {
+		params = os.Args[2:]
+	}
+	cmd := exec.Command(os.Args[1], params...)
+
+	// create pipes to read stderr and stdout
+	stderrReader, stderrWriter := io.Pipe()
+	stdoutReader, stdoutWriter := io.Pipe()
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = stderrWriter
+	cmd.Stderr = stdoutWriter
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go processStream(stderrReader, os.Stderr, &wg)
+	go processStream(stdoutReader, os.Stderr, &wg)
+
+	// start command
+	if err := cmd.Start(); err != nil {
+		write(err, os.Stderr)
+		os.Exit(1)
+	}
+
+	// wait until process is finished
+	exitStatus := 1
+	if err := cmd.Wait(); err != nil {
+		exitErr, ok := err.(*exec.ExitError)
+		if !ok {
+			write(err, os.Stderr)
+			exitStatus = 1
+		}
+		exitStatus = exitErr.ExitCode()
+	}
+
+	// close pipes
+	stderrWriter.Close()
+	stdoutWriter.Close()
+
+	// wait for go routiens that process stderr & stdout
+	wg.Wait()
+
+	// exit with same exitstatus as started application
+	os.Exit(exitStatus)
+}
